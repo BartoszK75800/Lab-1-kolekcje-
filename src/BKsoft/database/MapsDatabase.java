1dedb5fe2edaa8082ecbf6d27e51b48aceadabef
Bartosz K <atlf75800@gmail.com>
2021-11-23 22:12:18 +0100

    działające TreeSety i HashSety

--- src/BKsoft/database/MapsDatabase.java
+++ src/BKsoft/database/MapsDatabase.java
@@ -3,31 +3,43 @@ package BKsoft.database;
 import BKsoft.Mountain;
 import BKsoft.MountainEqHscode;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.TreeMap;
 
 public class MapsDatabase implements Database{
     private HashMap<Mountain, Integer> mountainHashMap = new HashMap<>();
-    //private TreeMap<Mountain, Integer> mountainTreeMap = new TreeMap<>();
+    private TreeMap<Mountain, Integer> mountainTreeMap = new TreeMap<>(new Comparator<Mountain>() {
+        @Override
+        public int compare(Mountain o1, Mountain o2) {
+            return 1;
+        }
+    });
     private HashMap<MountainEqHscode, Integer> mountainHashMapEH = new HashMap<>();
-    //private TreeMap<Mountain, Integer> mountainTreeMapEH = new TreeMap<>();
+    private TreeMap<MountainEqHscode, Integer> mountainTreeMapEH = new TreeMap<>(new Comparator<MountainEqHscode>() {
+        @Override
+        public int compare(MountainEqHscode o1, MountainEqHscode o2) {
+            return 1;
+        }
+    });
+
     Integer num=0;
 
     @Override
     public void addMountain(Mountain mountain, MountainEqHscode mtn) {
         this.num++;
         mountainHashMap.put(mountain, num);
-        //mountainTreeMap.put(mountain,num);
+        mountainTreeMap.put(mountain,num);
         mountainHashMapEH.put(mtn, num);
-        //mountainTreeMapEH.put(mtn, num);
+        mountainTreeMapEH.put(mtn, num);
     }
 
     @Override
     public void showDatabase() {
         System.out.println("HashMap: \n" + mountainHashMap.toString() + "\n");
-        //System.out.println("TreeMap: \n" + mountainTreeMap.toString() + "\n");
+        System.out.println("TreeMap: \n" + mountainTreeMap.toString() + "\n");
         System.out.println("HashMap (z funkcjami porownawczymi): \n" + mountainHashMapEH.toString() + "\n");
-        //System.out.println("TreeMap (z funkcjami porownawczymi):\n" + mountainTreeMapEH.toString() + "\n");
+        System.out.println("TreeMap (z funkcjami porownawczymi):\n" + mountainTreeMapEH.toString() + "\n");
     }
 
     @Override
@@ -36,3 +48,4 @@ public class MapsDatabase implements Database{
         mountainHashMapEH.remove(mtn);
     }
 }
+
--- src/BKsoft/database/SetDatabase.java
+++ src/BKsoft/database/SetDatabase.java
@@ -3,29 +3,40 @@ package BKsoft.database;
 import BKsoft.Mountain;
 import BKsoft.MountainEqHscode;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.TreeSet;
 
 public class SetDatabase implements Database{
     private HashSet<Mountain> mountainHashSet = new HashSet<>();
-    //private TreeSet<Mountain> mountainTreeSet = new TreeSet<>();
+    private TreeSet<Mountain> mountainTreeSet = new TreeSet<>(new Comparator<Mountain>() {
+        @Override
+        public int compare(Mountain o1, Mountain o2) {
+            return 1;
+        }
+    });
     private HashSet<MountainEqHscode> mountainHashSetEH = new HashSet<>();
-    //private TreeSet<MountainEqHscode> mountainTreeSetEH = new TreeSet<>();
+    private TreeSet<MountainEqHscode> mountainTreeSetEH = new TreeSet<>(new Comparator<MountainEqHscode>() {
+        @Override
+        public int compare(MountainEqHscode o1, MountainEqHscode o2) {
+            return 1;
+        }
+    });
 
     @Override
     public void addMountain(Mountain mountain, MountainEqHscode mtn) {
         mountainHashSet.add(mountain);
-        //mountainTreeSet.add(mountain);
+        mountainTreeSet.add(mountain);
         mountainHashSetEH.add(mtn);
-       // mountainTreeSetEH.add(mtn);
+        mountainTreeSetEH.add(mtn);
     }
 
     @Override
     public void showDatabase() {
         System.out.println("HashSet: \n" + mountainHashSet.toString() + "\n");
-        //System.out.println("TreeSet: \n" + mountainTreeSet.toString() + "\n");
+        System.out.println("TreeSet: \n" + mountainTreeSet.toString() + "\n");
         System.out.println("HashSet (z funkcjami porownawczymi): \n" + mountainHashSetEH.toString() + "\n");
-        //System.out.println("TreeSet: (z funkcjami porownawczymi):\n" + mountainTreeSetEH.toString() + "\n");
+        System.out.println("TreeSet: (z funkcjami porownawczymi):\n" + mountainTreeSetEH.toString() + "\n");
     }
 
     @Override
